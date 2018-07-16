8decc047ed24d77236c7e892d187a128470fe31f
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-07-16 21:43:42 +0300

    REST example with Client API

--- src/main/java/com/epam/web/resources/BeanResource.java
+++ src/main/java/com/epam/web/resources/BeanResource.java
@@ -1,13 +1,17 @@
 package com.epam.web.resources;
 
 import com.epam.xml.Food;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import lombok.val;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.*;
+import javax.ws.rs.ext.Providers;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static lombok.AccessLevel.PRIVATE;
 
@@ -15,14 +19,64 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE)
 public class BeanResource implements JsonRestfulWebResource {
 
+    @Context
+    Application app;
+
+    @Context
+    UriInfo uri;
+
+    @Context
+    HttpHeaders headers;
+
+    @Context
+    Request request;
+
+    @Context
+    SecurityContext security;
+
+    @Context
+    Providers providers;
+
+    // bean/123
     @GET
+    @Path("{id}")
     @Produces(APPLICATION_JSON)
-    public Response get() {
-        return ok(new Food(1, "waffles", "$4", "delicious!", 100));
+    public Response get(@PathParam("id") int id) {
+        return ok(new Food(id, "waffles", "$4", "delicious!", 100));
     }
 
-//    @POST
-//    public Response post() {
-//
-//    }
+    //webapi/bean/create
+    @POST
+    @Path("create")
+    @Consumes({APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_XML})
+    @Produces(APPLICATION_JSON)
+    public Response post(//Obj obj
+                         @FormParam("id") int id,
+                         @FormParam("name") String name,
+                         @CookieParam("JSESSIONID") String sessionId,
+                         @HeaderParam("X-My-Header") int x
+    ) {
+
+        val client = ClientBuilder.newClient();
+        String food = client
+                .target("http://localhost:8080/webapi/bean")
+                .path("{id}")
+                .resolveTemplate("id", id)
+                .request()
+                .get(String.class);
+
+        return ok(fromJSON(food, Food.class));
+
+//        return ok(new Obj(id, name, sessionId, x));
+    }
+}
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+class Obj {
+    int id;
+    String name;
+    String sessionId;
+    int x;
 }
